3c0d8c49ddc930a40bfd03abc19484ccd2a9ffea
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-02-02 15:24:06 -0400

    Fix

--- index.js
+++ index.js
@@ -12,54 +12,64 @@ firebase.initializeApp({
 	appId: "1:909520655494:web:f1f178d8e564789ea22d07"
 });
 const sleep = (time = 1000) => new Promise(resolve => setTimeout(() => resolve(time), time));
+const getCedula = (min, max)=>firebase.firestore().collection('CC').where('CC', '>=', min).where('CC', '<=', max).orderBy('CC','desc').limit(1).get().then(({ docs })=>{
+	return !docs.length?min:(docs[0].data().CC+1);
+});
 
-async function ScanTab({ Tab, CC, max, }) {
+
+
+
+async function ScanTab({ CC, waitFor=3000, Tab=null, Browser=null, response=null, error=null}){
 	console.clear();
-	if(CC > max) return process.exit("Se ha alcanzado el máximo de consultas para este rango");
-	console.log(`[Scaneando: ${CC}]`);
+	if(!Browser || (!Tab && !Browser)) process.exit(500);
 	try {
+		if(!Tab){
+			console.log('[Loading Page...] ');
+			const Tab = await Browser.newPage(),
+				url = "https://www.procuraduria.gov.co/portal/index.jsp?option=co.gov.pgn.portal.frontend.component.pagefactory.AntecedentesComponentPageFactory&action=consultar_antecedentes";
+			await Tab.goto(url,{ waitUntil:'load', timeout:30000 });
+		}
 		const Page = Tab.url().includes('apps.procuraduria.gov.co') ? Tab :
 			(await Tab.frames()).find(frame => frame.url().includes('apps.procuraduria.gov.co'));
-		
-		await Page.waitForSelector('#ddlTipoID', {
-			visible:true,
-			timeout: 30000,
-		});
-			
-		await Page.click('#ImageButton1');
-		console.log(`[Solving Captcha]`);
-		response = await Page.evaluate(() => document.querySelector('#txtRespuestaPregunta').value.trim());
-	
-		const question = await Page.evaluate(() => document.querySelector('#lblPregunta').innerText.toLowerCase());
-		console.log(`[Question]: `, question);
-		if (question.match(/cuanto es \d+ [\-\+\*\x\+] \d+/gi))
-			response = eval(question.replace(/^.*(\d+.*\d+).*$/gi, "$1").replace("x", "*"));
-		else if (question.match("es la capital de valle del cauca"))
-			response = "cali";
-		else if (question.match("capital del atlantico"))
-			response = "barranquilla";
-		else if (question.match("es la capital de ")) {
-			const city = question.replace(/^.*Capital de (\w+).*$/gi, "$1").toLowerCase()
-			response = city === 'colombia' ? 'bogota' : (
-				'antioquia' ? 'medellin' : null
-			);
-		}
-		if (Boolean(response))
-			await Page.evaluate((value) => document.querySelector('#txtRespuestaPregunta').value=value, response);
-		else {
-			await Page.click('#ImageButton1');
-			await sleep(2000);
-			return ScanTab(...arguments);
-		}
-		console.log(response);
-		console.log(`#############`);
 
-		await Page.evaluate((CC) => {
-			document.querySelector('#ddlTipoID').value = 1;
-			document.querySelector('#txtNumID').value = CC;
-			return document.querySelector('#btnConsultar').click();
-		}, CC);
+		await Page.waitForFunction(()=>{
+			const select = document.querySelector('#ddlTipoID')
+			if(!select) return false;
+			return select.value=1;
+		},{timeout: 30000});
 		
+		console.log('[Typing...] ', CC);
+		await Page.evaluate(CC=>document.querySelector('#txtNumID').value=CC, CC);
+		
+		if(!response){
+			// let response = await Page.evaluate(() => document.querySelector('#txtRespuestaPregunta').value.trim());
+			console.log(`[Solving Captcha]`);
+			const question = await Page.evaluate(()=>document.querySelector('#lblPregunta').innerText.toLowerCase());
+			console.log(`[Question]: `, question);
+			if (question.match(/cuanto es \d+ [\-\+\*\x\+] \d+/gi))
+				response = eval(question.replace(/^.*(\d+.*\d+).*$/gi, "$1").replace("x", "*"));
+			else if (question.match("es la capital de valle del cauca"))
+				response = "cali";
+			else if (question.match("capital del atlantico"))
+				response = "barranquilla";
+			else if (question.match("es la capital de ")) {
+				const city = question.replace(/^.*Capital de (\w+).*$/gi, "$1").toLowerCase()
+				response = city === 'colombia' ? 'bogota' : (
+					city === 'antioquia' ? 'medellin' : null
+				);
+			}
+			if (Boolean(response))
+				await Page.evaluate(value=>document.querySelector('#txtRespuestaPregunta').value=value,response);
+			else {
+				await Page.evaluate(()=>document.querySelector('#ddlTipoID').remove());
+				await Page.click('#ImageButton1');
+				return ScanTab({CC, waitFor, Tab, Browser});
+			}
+		}
+
+		await Page.click('#btnConsultar');
+		await sleep(2000);
+
 		console.log("[Waiting Response]");
 		await Page.waitForFunction(() => {
 			const cc = document.querySelector('#txtNumID').value;
@@ -77,40 +87,47 @@ async function ScanTab({ Tab, CC, max, }) {
 				data.querySelectorAll('span').forEach(span => names.push(span.innerText))
 			return names.length ? names : (error ? null : null);
 		});
-		console.log("[Client]: ", Client);
-		if (Client)
+
+		if (Client){
 			await firebase.firestore().collection('CC').doc(`CC_${CC}`).set({
 				CC,
 				name: Client.slice(0, -2),
 				lastname: Client.slice(-2),
 			});
+			CC++;
+		}
 		await sleep(3000);
 		return ScanTab({ Tab, CC: (CC + 1) });
 	}
-	catch (error) {}
-	finally { return ScanTab(...arguments); }
+	catch (_error) { error=true; }
+	finally {
+		if(error){
+			if(Tab) await Tab.close();
+			if (waitFor >= 180000){
+				await sleep(1800 * 1000);
+				return ScanTab({ CC, waitFor:3000, Browser,});
+			}
+			await sleep(waitFor *= 2);
+			return ScanTab({ CC, waitFor:3000, Browser,});
+		}
+		else process.exit(200);
+	}
 }
 
 
-module.exports = (async function Scrapping({
-	waitFor = 3000,
-	length = 100000,
-	step = process.argv.slice(2)[0] || 1,
-}) {
-	let Browser = null;
-	let errorMessage = null;
-	const max = ((step || 1) * length);
-	const min = step > 1 ? (((step - 1) * length) + 1) : 10;
-	const url = "https://www.procuraduria.gov.co/portal/index.jsp?option=co.gov.pgn.portal.frontend.component.pagefactory.AntecedentesComponentPageFactory&action=consultar_antecedentes";
+
+
+async function Scrapping({ waitFor = 3000, length = 100000, step = process.argv.slice(2)[0] || 1, }){
+	let Browser = null,
+		errorMessage = null;
+	const max = ((step || 1) * length),
+		min = step > 1 ? (((step - 1) * length) + 1) : 10,
 	try {
-		const CC = await firebase.firestore().collection('CC')
-			.where('CC', '>=', min)
-			.where('CC', '<=', max)
-			.orderBy('CC', 'desc').limit(1).get()
-			.then(({docs}) => (docs.length ? (docs[0].data().CC + 1) : min));
+		const CC = await getCedula(min, max);
+		if(CC > max) process.exit(200);
 		Browser = await puppeteer.launch({
-			headless: true,
 			// slowMo: 250,
+			headless: true,
 			// executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
 			args: [
 				'--disable-infobars',
@@ -122,20 +139,20 @@ module.exports = (async function Scrapping({
 				'--user-agent="' + (new userAgent()).toString() + '"',
 			]
 		});
-		const Tab = await Browser.newPage();
-		console.log(`[Loading Page For: ${CC}]`);
-		await Tab.goto(url, { waitUntil:'load', timeout:30000 });
-		return ScanTab({ Tab, CC, max });
+		return ScanTab({ CC, Browser, });
 	}
-	catch (error) { errorMessage = error; }
+	catch (error) { errorMessage=error; }
 	finally {
-		if(Browser) await Browser.close();
-		if (errorMessage) {
-			console.log(errorMessage);
-			if (waitFor >= 180000)
-				return process.exit(errorMessage);
+		if(errorMessage){
+			if(Browser) await Browser.close();
+			if (waitFor >= 180000){
+				await sleep(1800 * 1000);
+				return Scrapping({ step, length, waitFor:3000});
+			}
 			await sleep(waitFor *= 2);
-			return Scrapping({ waitFor, length, step, })
+			return Scrapping({ step, length, waitFor, });
 		}
+		else process.exit(200);
 	}
-})({});
+}
+module.exports = Scrapping({});
