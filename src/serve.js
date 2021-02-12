b4bcab1ae68f31f2f87adc61c11463d5fa3d0151
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-02-12 15:45:37 -0400

    up

--- src/serve.js
+++ src/serve.js
@@ -2,10 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const findClient = require('./plugins/cne')
+
+
 app.use(cors());
 app.get('/:cedula', async ({params:{cedula}}, res) =>{
     res.json(await findClient(cedula));
 });
-app.listen(8080, () => {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(8080, () => {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
+
+server = require('http').createServer(app)
+server.listen(80);
